dd030ed2c1bb7bfb40d6bcddd65a05fc6a8888f9
imrannaazir <imrannaaziremon@gmail.com>
2024-07-02 16:01:24 +0600

    [MODIFIED] - left side bar

--- client/src/routes/admin.routes.tsx
+++ client/src/routes/admin.routes.tsx
@@ -1,24 +1,9 @@
 import AddProduct from "@/pages/AddProduct";
 import HomePage from "@/pages/Home";
-import ProductDetails from "@/pages/ProductDetails";
 import ProductList from "@/pages/ProductList";
 // import UpdateProduct from "@/pages/UpdateProduct";
-import { BarChart2, Home, Images, ShoppingBag, User } from "lucide-react";
-import { IoMdPricetag } from "react-icons/io";
-import AddOrderPage from "@/pages/AddOrderPage";
+import { Award, BookOpen, Briefcase, Home, Layers } from "lucide-react";
 import { TPath } from "@/types";
-import AnalyticsPage from "../pages/Analytics";
-import AddCustomer from "@/pages/AddCustomer";
-import CustomerListPage from "@/pages/CustomerList";
-import AddImagePage from "@/pages/AddImage";
-import ImageListPage from "@/pages/ImageList";
-import AddCollectionPage from "@/pages/AddCollection";
-import CollectionListPage from "@/pages/CollectionList";
-import AddCategoryPage from "@/pages/AddCategory";
-import CategoryListPage from "@/pages/CategoryList";
-import BrandListPage from "@/pages/BrandList";
-import AddBrandsPage from "@/pages/AddBrand";
-import AddVariantPage from "@/pages/AddVariant";
 
 const paths: TPath[] = [
   {
@@ -32,22 +17,11 @@ const paths: TPath[] = [
     element: <HomePage />,
     label: "Home",
   },
+
   {
-    icon: <ShoppingBag className="mr-2 h-4 w-4" />,
-    label: "Orders",
-    path: "orders",
-    children: [
-      {
-        label: "Add",
-        path: "new",
-        element: <AddOrderPage />,
-      },
-    ],
-  },
-  {
-    icon: <IoMdPricetag className="mr-2 h-4 w-4" />,
-    label: "Product",
-    path: "products",
+    icon: <Layers className="mr-2 h-4 w-4" />,
+    label: "Project",
+    path: "projects",
     children: [
       {
         path: `new`,
@@ -59,89 +33,58 @@ const paths: TPath[] = [
         label: "List",
         element: <ProductList />,
       },
-      /* {
-        path: "update/:id",
-        element: <UpdateProduct />,
-      }, */
-      {
-        path: ":id",
-        element: <ProductDetails />,
-      },
     ],
   },
   {
-    icon: <User className="mr-2 h-4 w-4" />,
-    label: "Customers",
-    path: "customers",
+    icon: <Briefcase className="mr-2 h-4 w-4" />,
+    label: "Experiences",
+    path: "experiences",
     children: [
       {
-        path: "new",
+        path: `new`,
         label: "Add",
-        element: <AddCustomer />,
+        element: <AddProduct />,
       },
       {
-        path: "list",
+        path: `list`,
         label: "List",
-        element: <CustomerListPage />,
+        element: <ProductList />,
       },
     ],
   },
   {
-    icon: <Images className="mr-2 h-4 w-4" />,
-    label: "Contents",
-    path: "contents",
+    icon: <BookOpen className="mr-2 h-4 w-4" />,
+    label: "Blogs",
+    path: "blogs",
     children: [
       {
-        index: true,
-        element: <ImageListPage />,
+        path: `new`,
+        label: "Add",
+        element: <AddProduct />,
       },
       {
-        path: "add-image",
-        element: <AddImagePage />,
-      },
-      {
-        label: "Images",
-        path: "images",
-        element: <ImageListPage />,
-      },
-      {
-        path: "add-collection/:id",
-        element: <AddCollectionPage />,
-      },
-      {
-        label: "Collections",
-        path: "collections",
-        element: <CollectionListPage />,
-      },
-      {
-        path: "add-category/:id",
-        element: <AddCategoryPage />,
-      },
-      {
-        label: "Categories",
-        path: "categories",
-        element: <CategoryListPage />,
-      },
-      {
-        path: "add-brand/:id",
-        element: <AddBrandsPage />,
-      },
-      {
-        label: "Brands",
-        path: "brands",
-        element: <BrandListPage />,
-      },
-      {
-        path: "add-variant",
-        element: <AddVariantPage />,
+        path: `list`,
+        label: "List",
+        element: <ProductList />,
       },
     ],
   },
   {
-    icon: <BarChart2 className="mr-2 h-4 w-4" />,
-    label: "Analytics",
-    path: "analytics",
-    element: <AnalyticsPage />,
+    icon: <Award className="mr-2 h-4 w-4" />,
+    label: "Skills",
+    path: "skills",
+    children: [
+      {
+        path: `new`,
+        label: "Add",
+        element: <AddProduct />,
+      },
+      {
+        path: `list`,
+        label: "List",
+        element: <ProductList />,
+      },
+    ],
   },
 ];
 export default paths;
